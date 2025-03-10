7e7ca944e6c747eeff855f52d6c281c3e108ded8
Fu Minjie <fu.minjie@outlook.com>
2025-03-11 01:13:11 +0800

    Create data.R

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' 人类代谢酶数据库
+"enzyme_human"
+
+#' 小鼠代谢酶数据库
+"enzyme_mouse"
+
+#' 人类转运体数据库
+"transporter_human"
+
+#' 小鼠转运体数据库
+"transporter_mouse"
+
+#' 人类受体数据库
+"receptor_human"
+
+#' 小鼠受体数据库
+"receptor_mouse"
